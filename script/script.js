d4b85f7cd9a246f90969205bf7b696c9169298f5
splaka <syzeir.plaka@itis.pr.it>
2024-12-13 09:00:57 +0100

    Modifiche Spot 2

--- script/script.js
+++ script/script.js
@@ -22,3 +22,25 @@ document.addEventListener('click', (e) => {
         toggleButton.style.display = 'block'; // Show the button
     }
 });
+
+// Seleziona tutti gli elementi con classe "accordion"
+var acc = document.getElementsByClassName("accordion");
+
+// Aggiunge un evento click a ogni elemento
+for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        // Alterna la classe "active"
+        this.classList.toggle("active");
+
+        // Gestisce il pannello successivo
+        var panel = this.nextElementSibling;
+
+        // Mostra o nasconde il pannello
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null; // Chiude il pannello
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px"; // Mostra il pannello
+        }
+    });
+}
+
